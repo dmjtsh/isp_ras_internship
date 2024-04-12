b0a92f1dadd533c27bce3ff44894786614f1110c
dmjtsh <cyber.pelmen@yandex.ru>
2024-04-13 01:46:08 +0300

    working builder

--- tools/lr-mlir-opt/lr-mlir-opt.cpp
+++ tools/lr-mlir-opt/lr-mlir-opt.cpp
@@ -1,14 +1,17 @@
-#include "mlir/include/mlir/InitAllDialects.h"
-#include "mlir/include/mlir/Pass/PassManager.h"
-#include "mlir/include/mlir/Pass/PassRegistry.h"
-#include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "../../../mlir/IR/MLIRContext.h"
+#include "../../../mlir/InitAllDialects.h"
+#include "../../../mlir/InitAllPasses.h"
+#include "../../../mlir/Support/FileUtilities.h"
+#include "../../../mlir/Tools/mlir-opt/MlirOptMain.h"
+
+//#include "../../include/LifeRange/Passes.h"
 
 int main(int argc, char **argv) 
 {
+    //mlir::liferange::registerPasses();
+
     mlir::DialectRegistry registry;
     mlir::registerAllDialects(registry);
-  
-    mlir::PassRegistration<mlir::liferange::LifeRangePass>();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Life Range Pass Driver", registry));
