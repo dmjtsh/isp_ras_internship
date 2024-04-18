ab36fdf57f95feb13e6a7b78bb35937ee5327aaa
dmjtsh <cyber.pelmen@yandex.ru>
2024-04-18 22:40:10 +0300

    deleted unnecessary info from lit config

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,15 +1,9 @@
 import os
-import platform
-import re
-import subprocess
-import tempfile
 
 import lit.formats
 import lit.util
 
 from lit.llvm import llvm_config
-from lit.llvm.subst import ToolSubst
-from lit.llvm.subst import FindTool
 
 config.name = "LIFE_RANGE"
 
@@ -23,8 +17,6 @@ config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.life_range_obj_root, "test")
 config.life_range_tools_dir = os.path.join(config.life_range_obj_root, "bin")
 config.life_range_libs_dir = os.path.join(config.life_range_obj_root, "lib")
-
-config.substitutions.append(("%life_range_libs", config.life_range_libs_dir))
 
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
